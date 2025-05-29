0f402405a6e537da538656c3cb454c61b5c1e70e
lesley <lesleybanadzem2017@gmail.com>
2025-05-29 08:23:23 -0500

    Refactor CORS configuration to specify allowed origins and improve request handling

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,19 +11,25 @@ import (
 func main() {
 	app := fiber.New()
 
-	app.Use(cors.Config{
-		AllowOrigins: []string{"*"}, // URL for FE
-		AllowHeaders: []string{"Origin, Content-Type, Accept"},
-	})
+	var AllowOrigins = []string{"https://www.hello.com"}
+
+	app.Use(cors.New(cors.Config{
+		AllowCredentials: true,
+		AllowOrigins:     AllowOrigins, // URL for FE
+		AllowHeaders:     []string{"Origin, Content-Type, Accept"},
+		AllowMethods:     []string{fiber.MethodGet, fiber.MethodPost}, // REST Methods allowed
+		MaxAge:           3600,                                        // How long a preflight request should be cached for
+
+	}))
 	app.Get("/unprotected", func(c fiber.Ctx) error {
 		return c.JSON(map[string]string{
-			"message": "Welcomme to the unprotected route",
+			"message": "Welcome to the unprotected route",
 		})
 	})
 	app.Use(handlers.Authenticate)
 	app.Get("/welcome", func(c fiber.Ctx) error {
 		return c.JSON(map[string]string{
-			"message": "Welcomme to the home route",
+			"message": "Welcome to the home route",
 		})
 	})
 	routes.AuthRoutes(app)
