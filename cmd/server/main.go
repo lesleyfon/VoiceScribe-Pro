18ddd450ccd9a6641819e30058bbe0a45df85fd1
lesley <lesleybanadzem2017@gmail.com>
2025-05-29 07:51:21 -0500

    Created two routes to test if the routes work as expected.

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"voicescribe-pro/internal/api/handlers"
 	"voicescribe-pro/internal/api/routes"
 
 	"github.com/gofiber/fiber/v3"
@@ -9,7 +10,17 @@ import (
 func main() {
 	app := fiber.New()
 
-	routes.HelloWorld(app)
+	app.Get("/unprotected", func(c fiber.Ctx) error {
+		return c.JSON(map[string]string{
+			"message": "Welcomme to the unprotected route",
+		})
+	})
+	app.Use(handlers.Authenticate)
+	app.Get("/welcome", func(c fiber.Ctx) error {
+		return c.JSON(map[string]string{
+			"message": "Welcomme to the home route",
+		})
+	})
 	routes.AuthRoutes(app)
 
 	app.Listen(":3000")
