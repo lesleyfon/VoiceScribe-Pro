fb14d7da54b83b5e5a0b4964d7a0d1855ed2ca6c
lesley <lesleybanadzem2017@gmail.com>
2025-05-29 07:58:04 -0500

    Added basic config for CORS

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -5,11 +5,16 @@ import (
 	"voicescribe-pro/internal/api/routes"
 
 	"github.com/gofiber/fiber/v3"
+	"github.com/gofiber/fiber/v3/middleware/cors"
 )
 
 func main() {
 	app := fiber.New()
 
+	app.Use(cors.Config{
+		AllowOrigins: []string{"*"}, // URL for FE
+		AllowHeaders: []string{"Origin, Content-Type, Accept"},
+	})
 	app.Get("/unprotected", func(c fiber.Ctx) error {
 		return c.JSON(map[string]string{
 			"message": "Welcomme to the unprotected route",
