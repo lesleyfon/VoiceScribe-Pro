5a1cb3573b5432e318e25a7e075322b0032e1a3c
lesley <lesleybanadzem2017@gmail.com>
2025-11-18 11:08:45 -0600

    Implement database connection and add endpoint to retrieve notes

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -3,8 +3,10 @@ package main
 import (
 	"log"
 	"strings"
+	"time"
 	"voicescribe-pro/internal/api/middlewares"
 	"voicescribe-pro/internal/api/routes"
+	"voicescribe-pro/internal/db"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -15,8 +17,14 @@ import (
 func main() {
 	err := godotenv.Load()
 	if err != nil {
-		log.Println("Error loading env variables")
+		log.Print("Error loading env variables")
 	}
+
+	database, err := db.InitDB()
+	if err != nil {
+		log.Fatal("Error while connecting to DB: ", err)
+	}
+
 	app := fiber.New(
 		fiber.Config{
 			BodyLimit: 200 * 1024 * 1024, // 10MB in bytes
@@ -36,17 +44,38 @@ func main() {
 		MaxAge:           3600,         // How long a preflight request should be cached for
 
 	}))
+
 	app.Use(logger.New(logger.Config{
 		Format:     "${time} ${status} ${method} ${path}\n",
 		TimeFormat: "2006-01-02 15:04:05",
 		TimeZone:   "America/Chicago",
 	}))
+
 	app.Get("/health", func(c *fiber.Ctx) error {
 		return c.Status(fiber.StatusOK).JSON(fiber.Map{
 			"status": "healthy",
 		})
 	})
 
+	type NoteResponse struct {
+		ID        uint      `json:"ID"`
+		CreatedAt time.Time `json:"CreatedAt"`
+		Title     string    `json:"Title"`
+	}
+
+	app.Get("/notes", func(c *fiber.Ctx) error {
+		var notes []NoteResponse
+		result := database.Model(&db.NotesStruct{}).Select("id", "created_at", "title").Find(&notes)
+
+		if result.Error != nil {
+			return c.Status(fiber.StatusInternalServerError).JSON(fiber.Map{
+				"status": "error",
+				"error":  result.Error.Error(),
+			})
+		}
+
+		return c.Status(fiber.StatusOK).JSON(fiber.Map{"notes": notes})
+	})
 	app.Use(middlewares.Authenticate())
 
 	// Routes
